1f5c6a0c750b7d51c69249e15a90fbb86f363c0a
REHXZ <rehxxz@gmail.com>
2024-04-29 11:58:54 +0800

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import StarsCanvas from "@/components/main/StarBackground";
+import HeroContent from "@/components/sub/HeroContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,8 +19,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}>
+        
         <StarsCanvas/>
-
         {children}</body>
     </html>
   );
